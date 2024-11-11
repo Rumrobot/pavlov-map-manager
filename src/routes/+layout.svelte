8bfd920c23bd5cb54f5e1c206b5ca57a2f77e21d
Nils-Erik <rumrobot@gmail.com>
2024-11-11 15:23:36 +0100

    feat: Custom titlebar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,18 +5,20 @@
 	import { Toaster } from '$lib/components/ui/sonner';
 	import { getCurrentWindow } from '@tauri-apps/api/window';
 	import { Button } from '$components/ui/button';
-	import { X, Minus, Maximize, Minimize, Menu, RotateCw } from '@o7/icon/lucide';
+  import { Fullscreen, FullscreenExit, Close, Remove, Menu } from '@o7/icon/material';
+  import { RefreshCcw, Server } from '@o7/icon/lucide';
 	import { goto } from '$app/navigation';
+  import { page } from '$app/stores';
 	import { onMount } from 'svelte';
-
-	const Window = getCurrentWindow();
-	let maximized = $state(false);
+  import { cn } from '$lib/utils';
 
 	const { children } = $props();
-	$inspect(maximized).with(console.log);
+	const Window = getCurrentWindow();
+	let maximized = $state(false);
+  let reload = $state(false);
+
 	const resize = async () => {
 		maximized = await Window.isMaximized();
-		console.log(maximized);
 	}
 
 	onMount(async () => {
@@ -28,37 +30,46 @@
 <Toaster position="top-right" />
 <svelte:window on:resize={resize} />
 
-<div class="fixed top-0 border-b w-full h-12 flex justify-between items-center bg-background/75 backdrop-blur"
+<div class="fixed top-0 border-b w-full h-12 grid grid-cols-5 justify-between items-center bg-background/75 backdrop-blur"
 		 data-tauri-drag-region>
-	<div class="flex items-center">
-		<Button variant="ghost" size="icon" class="rounded-none" onclick={() => goto("/")}>
-			<ModIoCog />
+	<div class="flex items-center ml-2 gap-1 col-span-2" data-tauri-drag-region>
+		<Button variant="ghost" size="icon" onclick={() => {if ($page.url.pathname !== "/") goto("/")}} class={cn("hover:bg-inherit", $page.url.pathname === "/" ? "hover:cursor-default" : "text-muted")}>
+			<ModIoCog size={32} />
 		</Button>
-		<Button variant="ghost" size="icon" class="rounded-none">
-			<RotateCw />
+    <Button variant="ghost" size="icon" onclick={() => {if ($page.url.pathname !== "/servers") goto("/servers")}} class={cn("hover:bg-inherit", $page.url.pathname === "/servers" ? "hover:cursor-default" : "text-muted")}>
+      <Server size={24} />
+    </Button>
+    <div class="h-8 bg-border w-[1px]"></div>
+		<Button variant="ghost" size="icon" onclick={() => reload = true}>
+			<RefreshCcw class={`animate-spin repeat-1 direction-reverse ease-in ${reload ? "running" : "paused"}`} />
 		</Button>
 	</div>
-	<div class="flex items-center">
-		<Button variant="ghost" size="icon" class="rounded-none">
+  <p class="text-sm text-muted pointer-events-none justify-self-center" data-tauri-drag-region>
+  Pavlov Map Manager
+  </p>
+	<div class="flex items-center h-full col-span-2 justify-self-end" data-tauri-drag-region>
+		<Button variant="ghost" size="icon" class="mr-1">
 			<Menu />
 		</Button>
-		<Button variant="ghost" size="icon" class="rounded-none" onclick={() => Window.minimize()}>
-			<Minus />
-		</Button>
-		<Button variant="ghost" size="icon" class="rounded-none" onclick={() => Window.toggleMaximize()}>
-			{#if maximized}
-				<Minimize />
-			{:else}
-				<Maximize />
-			{/if}
-		</Button>
-		<Button variant="ghost" size="icon" class="rounded-none hover:bg-destructive" onclick={() => Window.close()}>
-			<X />
-		</Button>
+    <div class="h-full bg-border w-[1px]"></div>
+    <div class="h-full flex text-muted">
+      <Button variant="ghost" class="px-4 rounded-none h-full" onclick={() => Window.minimize()}>
+        <Remove size={18} />
+      </Button>
+      <Button variant="ghost" class="px-4 rounded-none h-full" onclick={() => Window.toggleMaximize()}>
+        {#if maximized}
+          <FullscreenExit size={18} />
+        {:else}
+          <Fullscreen size={18} />
+        {/if}
+      </Button>
+      <Button variant="ghost" class="px-4 rounded-none h-full hover:bg-destructive" onclick={() => Window.close()}>
+        <Close size={18} />
+      </Button>
+    </div>
 	</div>
 </div>
 
-<!-- h-[calc(100%-50px)] can be used, if the site has a navbar -->
 <div class="mt-12">
 	{@render children()}
 </div>
